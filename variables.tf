14aae86b71ebdb4fdcd84c8fac3359f99fc6334b
jreb-123 <jrebazatorres@gmail.com>
2026-02-03 16:51:53 +0100

    Add variables for GCP project configuration

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# terraform/variables.tf
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
