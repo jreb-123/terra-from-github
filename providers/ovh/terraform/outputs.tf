88ae1a189f9f875150bac78a9b1d2265ff74c564
Carlos Perez <cperez@pereza.com>
2026-02-05 14:07:59 +0000

    fix: usar provider OVH nativo en lugar de OpenStack01

--- providers/ovh/terraform/outputs.tf
+++ providers/ovh/terraform/outputs.tf
@@ -1,26 +1,16 @@
 output "master_public_ips" {
   description = "Public IPs of master nodes"
-  value       = openstack_networking_floatingip_v2.k3s_master_fip[*].address
-}
-
-output "master_private_ips" {
-  description = "Private IPs of master nodes"
-  value       = openstack_compute_instance_v2.k3s_master[*].access_ip_v4
+  value       = ovh_cloud_project_instance.k3s_master[*].ip_address
 }
 
 output "worker_public_ips" {
   description = "Public IPs of worker nodes"
-  value       = openstack_networking_floatingip_v2.k3s_worker_fip[*].address
-}
-
-output "worker_private_ips" {
-  description = "Private IPs of worker nodes"
-  value       = openstack_compute_instance_v2.k3s_worker[*].access_ip_v4
+  value       = ovh_cloud_project_instance.k3s_worker[*].ip_address
 }
 
 output "k3s_api_endpoint" {
   description = "K3s API endpoint"
-  value       = "https://${openstack_networking_floatingip_v2.k3s_master_fip[0].address}:6443"
+  value       = length(ovh_cloud_project_instance.k3s_master) > 0 ? "https://${ovh_cloud_project_instance.k3s_master[0].ip_address}:6443" : ""
 }
 
 # Generar inventory de Ansible
@@ -28,12 +18,12 @@ output "ansible_inventory" {
   description = "Ansible inventory in YAML format"
   value = templatefile("${path.module}/templates/inventory.tpl", {
     master_ips  = zipmap(
-      openstack_compute_instance_v2.k3s_master[*].name,
-      openstack_networking_floatingip_v2.k3s_master_fip[*].address
+      ovh_cloud_project_instance.k3s_master[*].name,
+      ovh_cloud_project_instance.k3s_master[*].ip_address
     )
     worker_ips = zipmap(
-      openstack_compute_instance_v2.k3s_worker[*].name,
-      openstack_networking_floatingip_v2.k3s_worker_fip[*].address
+      ovh_cloud_project_instance.k3s_worker[*].name,
+      ovh_cloud_project_instance.k3s_worker[*].ip_address
     )
   })
 }
